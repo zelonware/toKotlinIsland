d8f8a9c09c9d121cf996f1d2d7c2dea385175a5b
Elena G <elena.guzbla@gmail.com>
2017-07-29 12:30:41 +0200

    Create input.kt

---
+++ ejemplos/input.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {    
+    println("Dame un número: ")
+    
+    val lector = Scanner(System.`in`)
+    val numero = lector.nextInt()
+    
+    println("Tu número es $numero")
+}
